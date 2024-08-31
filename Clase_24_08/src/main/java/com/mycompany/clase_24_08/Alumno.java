28dcbde996423ec1b9698c128d7d9ac3ace3fda1
MrNarnia01 <brunonp03@gmail.com>
2024-08-31 10:54:04 -0300

    Aclaraciones y comentarios de la ultima clase

--- Clase_24_08/src/main/java/com/mycompany/clase_24_08/Alumno.java
+++ Clase_24_08/src/main/java/com/mycompany/clase_24_08/Alumno.java
@@ -3,21 +3,21 @@ package com.mycompany.clase_24_08;
 import javax.swing.JOptionPane;
 
 public class Alumno {
-    
+    //Se crean los atributos
     private String nombre;
     private int edad;
     
-    
+    //Constructor sin parametros
     Alumno(){
-        this.nombre="nada";
+        this.nombre="";
         this.edad=0;
     }
-    
+    //Constructor con parametros
     Alumno(String n, int e){
         this.nombre=n;
         this.edad=e;
     }
-    
+    //Getters y Setters
     public String getNombre() {
         return nombre;
     }
@@ -33,13 +33,14 @@ public class Alumno {
     public void setEdad(int edad) {
         this.edad = edad;
     }
-    
+    //Metodos a gusto del programador
+    //Este metodo es para registrar los datos y guardarlos en el objeto
     public void registrar(){
         this.nombre=JOptionPane.showInputDialog("Ingrese nombre");
         String e=JOptionPane.showInputDialog("Ingrese edad");
         this.edad = Integer.parseInt(e);
     }
-    
+    //Este metodo devuelve ordenado los datos de un alumno
     public String mostrar(){
         String a="";
         a+="Nombre: "+this.nombre;
--- Clase_24_08/src/main/java/com/mycompany/clase_24_08/Clase_24_08.java
+++ Clase_24_08/src/main/java/com/mycompany/clase_24_08/Clase_24_08.java
@@ -5,13 +5,18 @@ import javax.swing.JOptionPane;
 public class Clase_24_08 {
 
     public static void main(String[] args) {
+        /*
+            *Aclaracion: JOptionPane se usa para mostrar 
+                    o pedir info en pantalla tipo pop-up
+        */
         
+        //Prueba del metodo contructor sin parametros
         Alumno alumno= new Alumno();
         JOptionPane.showMessageDialog(null, alumno.mostrar());
-           
+        //Prueba del metodo contructor con parametros
         alumno= new Alumno("juan",22);
         JOptionPane.showMessageDialog(null, alumno.mostrar());
-        
+        //Prueba del metodo registrar
         alumno.registrar();
         JOptionPane.showMessageDialog(null, alumno.mostrar());
         

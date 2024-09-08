1553f1d39ce95a548cb3bc679a4a6a25a6fd5fc6
MrNarnia01 <brunonp03@gmail.com>
2024-09-08 16:27:57 -0300

    Clase 07 09 - Actualizacion

--- Clase_07_09/src/clase_07_09/Clase_07_09.java
+++ Clase_07_09/src/clase_07_09/Clase_07_09.java
@@ -2,6 +2,7 @@ package clase_07_09;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JOptionPane;
 
 
@@ -32,15 +33,15 @@ public class Clase_07_09 {
         
         do{
             op=JOptionPane.showInputDialog("""
-                                           1-Registrar defuncion
-                                           2-Modificar datos por nombre y especie
-                                           3-Eliminar por nombre y especie
-                                           4-Listar perro mas longevo
-                                           5-Listar edad de los conejos
-                                           6-Listar datos de perros fallecidos en el mes actual
-                                           7-Gatos por fecha de nacimiento
-                                           8-Listado por especie
-                                           9-Salir""");
+                                           -1-Registrar defuncion
+                                           -2-Modificar datos por nombre y especie
+                                           -3-Eliminar por nombre y especie
+                                           -4-Listar perro mas longevo
+                                           -5-Listar edad de los conejos
+                                           -6-Listar datos de perros fallecidos en el mes actual
+                                           -7-Gatos por fecha de nacimiento
+                                           -8-Listado por especie
+                                           -9-Salir""");
             switch(Integer.parseInt(op)){
                 case 1:
                     l=bus();
@@ -68,19 +69,75 @@ public class Clase_07_09 {
                     }else JOptionPane.showMessageDialog(null, "Error");
                 break;
                 case 4:
-                    
+                    l=-1;
+                    v="";
+                    for (int i = 0; i < n; i++){
+                        if(vm[i].getCod()!=-1){
+                            if(vm[i].getEsp().equalsIgnoreCase("perro")){
+                                if(l==-1)   l=i;
+                                else if(vm[l].edad()<vm[i].edad()){
+                                    l=i;
+                                }
+                            }
+                        }
+                    }
+                    if(l==-1)   JOptionPane.showMessageDialog(null, "Error");
+                    else{
+                        for (int i = 0; i < n; i++){
+                            if(vm[i].getCod()!=-1){
+                                if(vm[i].getEsp().equalsIgnoreCase("perro") && vm[l].edad()==vm[i].edad()){
+                                    v+=vm[i].mostrar();
+                                }
+                            }
+                        }
+                        JOptionPane.showMessageDialog(null, v);
+                    }
                 break;
                 case 5:
-                    
+                    v="";
+                    for (int i = 0; i < n; i++){
+                        if(vm[i].getCod()!=-1){
+                            if(vm[i].getEsp().equalsIgnoreCase("conejo")){
+                                v+="\n\nId: "+vm[i].getCod()+"      Edad: "+vm[i].edad();
+                            }
+                        }
+                    }
+                    if(v.equalsIgnoreCase("")){
+                        JOptionPane.showMessageDialog(null, "Error");
+                    }else JOptionPane.showMessageDialog(null,"Listado de edad de conejos\n\n" + v);
                 break;
                 case 6:
-                    
+                    Date hoy = new Date();
+                    for (int i = 0; i < n; i++){
+                        if(vm[i].getCod()!=-1 && vm[i].getfDef()!=null){
+                            if(vm[i].getEsp().equalsIgnoreCase("perro") && 
+                                    vm[i].getfDef().getYear()==hoy.getYear() &&
+                                    vm[i].getfDef().getMonth()==hoy.getMonth()){
+                                v+=vm[i].mostrar();
+                            }
+                        }
+                    }
+                    if(v.equalsIgnoreCase("")){
+                        JOptionPane.showMessageDialog(null, "Error");
+                    }else JOptionPane.showMessageDialog(null,"Listado de perros fallecidos en el ultimo mes\n\n" + v);
                 break;
                 case 7:
+                    v="";
+                    ordFecha();
+                    v=listEsp("gato");
+                    
+                    if(v.equalsIgnoreCase("")){
+                        JOptionPane.showMessageDialog(null, "Error");
+                    }else JOptionPane.showMessageDialog(null,"Listado de gatos por fecha de nacimiento\n\n" + v);
                     
                 break;
                 case 8:
-                    
+                    v=listEsp("perro");
+                    JOptionPane.showMessageDialog(null,"Listado de perros\n\n" + v);
+                    v=listEsp("gato");
+                    JOptionPane.showMessageDialog(null,"Listado de gatos\n\n" + v);
+                    v=listEsp("conejo");
+                    JOptionPane.showMessageDialog(null,"Listado de conejos\n\n" + v);
                 break;
                 case 9:
                 break;
@@ -128,5 +185,31 @@ public class Clase_07_09 {
         return b;
     }
     
+    public static void ordFecha(){
+        Mascota aux;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                if(vm[i].getCod()!=-1 && vm[j].getCod()!=-1){
+                    if(vm[i].getfNac().compareTo(vm[j].getfNac())>0){
+                        aux=vm[i];
+                        vm[i]=vm[j];
+                        vm[j]=aux;
+                    }
+                }
+            }
+        }
+    }
+    
+    public static String listEsp(String esp){
+        String m="";
+        for (int i = 0; i < n; i++) {
+            if(vm[i].getCod()!=-1){
+                if(vm[i].getEsp().equalsIgnoreCase(esp)){
+                    m+=vm[i].mostrar();
+                }
+            }
+        }
+        return m;
+    } 
     
 }
